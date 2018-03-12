b3b2945244b0c8b6d1634a408c3601a22864e93d
Hugh_weinbridge <hugh_weinbridge@BigAls-MacBook-Pro.local>
2018-03-11 23:24:51 -0700

    adding tle class library
    tle class is for processing and storing tle data relevant to a single
    tle. This object can help to manipulate tle parameters and generate new
    test tle’s.

---
+++ tle.py
@@ -0,0 +1,15 @@
+### TLE Library
+
+class tle(object):
+'''Class tle: is a container 
+'''
+	def __init__(self):
+		return 0;
+	def import_tle(self):
+		#import from TLE plaintext file
+		return 0
+	def export_tle(self):
+		#As plaintext to file, to csv, or as pandas.DataFrame
+		return 0;
+	def modify(self):
+		#modifies one or more TLE parameters and yields a new tle object
